e054d766b5656eec405e8aaf22e2b93164e5225c
student <iamdrzazgowski@gmail.com>
2023-02-28 14:52:57 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Dobrze robią");
+        System.out.println("Dobrze robią xxx");
     }
 }
-//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
+
